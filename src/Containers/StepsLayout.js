aa6a415bab8658765d824eb685d6faed0598da85
CinLo82 <cinlos3382@gmail.com>
2023-08-21 18:38:01 -0300

    se agregan los archivos con los que cuenta el proyecto

---
+++ src/Containers/StepsLayout.js
@@ -0,0 +1,17 @@
+import { Welcome } from '../Components/Welcome'
+import { Search } from '../Components/Search'
+import { Passengers } from '../Components/Passengers'
+import { Tickets } from '../Components/Tickets'
+import './StepsLayout.css'
+
+export const StepsLayout = ({ state, send }) => {
+  const renderContent = () => {
+    return <Welcome />
+  }
+
+  return (
+    <div className='StepsLayout'>
+      {renderContent()}
+    </div>
+  )
+}
